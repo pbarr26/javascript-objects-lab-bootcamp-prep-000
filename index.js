0b6af8c042a8b017d3fe3d365bbc4372cdcd319a
cerebral-map-4184 <patriciagbarrett@gmail.com>
2019-01-20 21:35:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 
 function deleteFromObjectByKey(object, key) {
   var newObj = Object.assign({}, object)
-  delete newObj.key
+  delete object.key
   return newObj
 }
