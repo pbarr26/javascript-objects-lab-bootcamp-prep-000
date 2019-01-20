d51b117c5b2f052f1eb35f5c09db9523e0c694d7
cerebral-map-4184 <patriciagbarrett@gmail.com>
2019-01-20 21:18:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,3 @@ var recipes= {}
 function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign({}, obj, { [key]: value })
 }
-
-//  1) Objects updateObjectWithKeyAndValue(object, key, value)
-//returns an object with the orignal key value pairs and the new key value pair:
